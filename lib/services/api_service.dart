ab0f17d04c51b8ec0ad1137d0c8a48e02e46da8c
alphaAntarik <antarikpanditison@gmail.com>
2023-11-26 07:16:45 +0530

    second

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -9,10 +9,6 @@ class ApiServices {
     final http.Response response = await http.get(
       Uri.parse(
           'https://api.themoviedb.org/3/movie/$item?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed'),
-      // headers: {
-      //   'X-RapidAPI-Key': '46f7dffe1amsh1715f22da088b7bp195c52jsn2014f61ad5fc',
-      //   'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-      // },
     );
 
     if (response.statusCode == 200) {
